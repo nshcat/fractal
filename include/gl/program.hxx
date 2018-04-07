0cceafe720b32cdd519efb003a4abe5675b7b32b
nshcat <nshcat@users.noreply.github.com>
2018-04-07 12:45:26 +0200

    Added OpenGL utilities

---
+++ include/gl/program.hxx
@@ -0,0 +1,142 @@
+#pragma once
+
+#include <stdexcept>
+#include <GLXW/glxw.h>
+#include <ut/format.hxx>
+#include <log.hxx>
+#include "shader.hxx"
+
+namespace fractal
+{
+	namespace gl
+	{
+		struct defer_creation_t{ };
+		
+		constexpr defer_creation_t defer_creation{ };
+
+		// Shader program abstraction
+		class program final
+		{
+			public:
+				template< internal::shader_type... Types >
+				program(const internal::shader<Types>&... p_shaders)
+					: program()
+				{
+					auto x = { (this->attach(p_shaders),0)... }; 
+					(void)x;
+					
+					link();
+					
+					
+				}
+				
+				program()
+					: m_Handle{glCreateProgram()}
+				{
+					if(!m_Handle)
+						throw ::std::runtime_error("Failed to create program!");
+				}
+				
+				program(defer_creation_t)
+					: m_Handle{ }
+				{
+				}
+				
+			public:
+				~program()
+				{
+					if(m_Handle)
+					{
+						glDeleteProgram(m_Handle);
+					}
+				}
+			
+				program(const program&) = delete;
+				program& operator=(const program&) = delete;
+				
+				/*program(program&&) = default;
+				program& operator=(program&&) = default;*/
+				
+				program(program&& p_prog)
+					: m_Handle{ }
+				{
+					::std::swap(this->m_Handle, p_prog.m_Handle);
+				}
+				
+				program& operator=(program&& p_prog)
+				{
+					if(this->m_Handle)
+						glDeleteProgram(this->m_Handle);
+						
+					this->m_Handle = { };
+				
+					::std::swap(this->m_Handle, p_prog.m_Handle);
+				
+					return *this;
+				}
+			
+			
+			public:
+				template< internal::shader_type Type >
+				auto attach(const internal::shader<Type>& p_shader)
+					-> void
+				{
+					glAttachShader(m_Handle, p_shader.handle());
+				}
+				
+				auto link()
+					-> void
+				{
+					glLinkProgram(m_Handle);
+					
+					if(query(GL_LINK_STATUS) == GL_FALSE)
+						throw shader_exception("Failed to link shader program!", info_log());
+				}
+				
+				auto handle() const
+					-> GLuint
+				{
+					return m_Handle;
+				}
+				
+				auto use() const
+					-> void
+				{	
+					glUseProgram(m_Handle);
+				}
+				
+				auto query(GLenum p_name) const
+					-> GLint
+				{
+					GLint t_result;
+					glGetProgramiv(handle(), p_name, &t_result);
+					return t_result;
+				}
+				
+				auto info_log() const
+					-> ::std::string
+				{
+					// Query info log len
+					auto t_len = query(GL_INFO_LOG_LENGTH);
+						
+					if(t_len == 0) return {};
+						
+					// Create buffer
+					::std::vector<char> t_buf(t_len+1);
+						
+					// Fetch info log
+					GLsizei t_result_length{};
+					glGetProgramInfoLog(	m_Handle,
+											t_len,
+											&t_result_length,
+											t_buf.data()
+										);
+											
+					return{ t_buf.begin(), t_buf.end() };		
+				}
+		
+			private:
+				GLuint m_Handle{ };
+		};
+	}
+}
---
+++ include/gl/uniform.hxx
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <stdexcept>
+#include <utility>
+#include <GLXW/glxw.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <ut/array_view.hxx>
+#include <ut/string_view.hxx>
+#include <ut/always_false.hxx>
+#include "program.hxx"
+
+namespace fractal
+{
+	namespace gl
+	{
+		namespace internal
+		{
+			template< typename T >
+			auto set_uniform_impl(GLint, const T&)
+				-> void
+			{
+				static_assert(ut::always_false_v<T>, "Given type is not supported by set_uniform!");
+			}
+				
+				
+			#define MAKE_UNIFORM_IMPL(__type) 											\
+			template< >																	\
+			inline auto set_uniform_impl<__type>(GLint p_location, const __type& p_value)\
+				-> void																
+					
+			#define MAKE_UNIFORM_IMPL_VEC(__type, __suffix)								\
+			MAKE_UNIFORM_IMPL(__type)													\
+			{																			\
+				glUniform##__suffix ( p_location, 1, glm::value_ptr(p_value) );			\
+			}
+			
+			#define MAKE_UNIFORM_IMPL_BVEC(__length)									\
+			MAKE_UNIFORM_IMPL(glm::bvec##__length)										\
+			{																			\
+				glm::uvec##__length t_tmp(p_value);										\
+				glUniform##__length##uiv ( p_location, 1, glm::value_ptr(t_tmp) );		\
+			}
+			
+			#define MAKE_UNIFORM_IMPL_SQUARE_MAT(__type, __suffix)						\
+			MAKE_UNIFORM_IMPL(__type)													\
+			{																			\
+				glUniform##__suffix ( p_location, 1, false, glm::value_ptr(p_value) );	\
+			}
+				
+			#define MAKE_UNIFORM_IMPL_SCALAR(__type, __suffix)							\
+			MAKE_UNIFORM_IMPL(__type)													\
+			{																			\
+				glUniform##__suffix ( p_location, p_value );							\
+			}
+			
+			
+			MAKE_UNIFORM_IMPL_VEC(glm::vec2, 2fv)
+			MAKE_UNIFORM_IMPL_VEC(glm::vec3, 3fv)
+			MAKE_UNIFORM_IMPL_VEC(glm::vec4, 4fv)
+			
+			MAKE_UNIFORM_IMPL_VEC(glm::ivec2, 2iv)
+			MAKE_UNIFORM_IMPL_VEC(glm::ivec3, 3iv)
+			MAKE_UNIFORM_IMPL_VEC(glm::ivec4, 4iv)
+			
+			MAKE_UNIFORM_IMPL_VEC(glm::uvec2, 2uiv)
+			MAKE_UNIFORM_IMPL_VEC(glm::uvec3, 3uiv)
+			MAKE_UNIFORM_IMPL_VEC(glm::uvec4, 4uiv)
+			
+			MAKE_UNIFORM_IMPL_BVEC(2)
+			MAKE_UNIFORM_IMPL_BVEC(3)
+			MAKE_UNIFORM_IMPL_BVEC(4)
+			
+			MAKE_UNIFORM_IMPL_SQUARE_MAT(glm::mat2, Matrix2fv)
+			MAKE_UNIFORM_IMPL_SQUARE_MAT(glm::mat3, Matrix3fv)
+			MAKE_UNIFORM_IMPL_SQUARE_MAT(glm::mat4, Matrix4fv)
+			
+			MAKE_UNIFORM_IMPL_SCALAR(unsigned int, 1ui)
+			MAKE_UNIFORM_IMPL_SCALAR(int, 1i)
+			MAKE_UNIFORM_IMPL_SCALAR(bool, 1ui)
+			MAKE_UNIFORM_IMPL_SCALAR(float, 1f)
+			
+			#undef MAKE_UNIFORM_IMPL_SCALAR
+			#undef MAKE_UNIFORM_IMPL_SQUARE_MAT
+			#undef MAKE_UNIFORM_IMPL_BVEC
+			#undef MAKE_UNIFORM_IMPL_VEC
+			#undef MAKE_UNIFORM_IMPL
+		}
+		
+		template< typename T >
+		auto set_uniform(GLint p_location, const T& p_value)
+			-> void
+		{
+			internal::set_uniform_impl<T>(p_location, p_value);
+		}
+
+		template< typename T >
+		auto set_uniform(const program& p_prog, ut::string_view p_name, const T& p_value)
+			-> void
+		{
+			// Get location. We need a zero-terminated string here.
+			auto t_str = p_name.to_string();
+			auto t_loc = glGetUniformLocation(p_prog.handle(), t_str.c_str());
+		
+			if(t_loc != -1)
+			{
+				set_uniform<T>(t_loc, p_value);
+			}
+			//else throw ::std::runtime_error("Invalid uniform name for given program!");
+		}
+	}
+}
